6dd90a51f075e45e79d11b9d99365d10d9ef8f39
ybouhjira <b.youssef91@gmail.com>
2014-06-11 17:44:32 +0100

    User entity

---
+++ src/Ilis/UsersBundle/Entity/User.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace Ilis\UsersBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+/**
+ * User
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class User implements UserInterface, \Serializable
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /*
+    * @ORM\ManyToMany(targetEntity="Role", inversedBy="users")
+    */
+    private $roles;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="isActive", type="boolean")
+     */
+    private $isActive;
+
+
+    public function __construct()
+    {
+      $this->isActive = true;
+      $this->roles = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set isActive
+     *
+     * @param boolean $isActive
+     * @return User
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    /**
+     * Get isActive
+     *
+     * @return boolean 
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+
+    public function getSalt() {
+      // We don't need salt because we use bcrypt  
+      return null;
+    }
+
+    public function getRoles() 
+    {
+      return $this->roles->toArray();
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+    public function serialize()
+    {
+      return serialize(array(
+        $this->id,
+        $this->email,
+        $this->password,
+        )
+      );
+    }
+
+    public function unserialize($serialized)
+    {
+      list(
+        $this->id,
+        $this->username,
+        $this->password,
+        ) = unserialize($serialized);
+    }
+}
